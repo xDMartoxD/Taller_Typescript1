7d9152051467766b45c2a24a771509c7786cd4c1
Martin <m.restrepom@uniades.edu.co>
2021-04-07 22:33:41 -0500

    Taller 1

---
+++ dataCourses.ts
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Ingeniería de Sw", "Pablo Picasso", 4),
+  new Course("Futbol 1", "Freddy Rincón", 2),
+  new Course("Algoritmos", "Carlos Fuentes", 2),
+  new Course("Estructuras de Datos", "Yesid D", 1),
+  new Course("Futbol 2", "James R", 6)
+]
